2c26afa81e495a634e6b0a78fa2b8084f44390a0
Mónica Pérez Martín <monica.p.m@outlook.com>
2018-11-28 06:45:03 -0600

    Initial commit

---
+++ serverCode.js
@@ -0,0 +1,9 @@
+var redis = require('redis');
+var client = redis.createClient();
+
+client.on("error", funtion (err) {
+    console.log("Error " + err);
+});
+
+client.set("Name", "John", redis.print);
+client.hset("HashValues", "HashKey1", "100", redis.print);
